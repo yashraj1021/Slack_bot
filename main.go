46f4fe833dca490310ab2930806c836aca69f8a6
yashraj1021 <yashrajthakur1021@gmail.com>
2023-07-26 02:25:45 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK-BOT-TOKEN", "xoxb-5660323445328-5649346785889-QUlGSJUm8Eusjoa65gq5oGQS")
+	os.Setenv("CHANNEL_ID", "C05K37EQGJV")
+	api := slack.New(os.Getenv("SLACK-BOT-TOKEN"))
+	channelArr := []string{os.Getenv("Channel_ID")}
+	fileArr := []string{"ssc marksheet signed.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s, caused error", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
